c734fe916f83483b9a50090d1a6f37a385106dcf
AArinova <aliya-arinova@yandex.ru>
2024-08-26 14:31:27 +0700

    Add files via upload

--- module_5_3.py
+++ module_5_3.py
@@ -26,20 +26,39 @@ class House:
         self.__add__( other)
         return self
 
+    def __radd__(self, other):
+        self.__add__( other)
+        return self
+
     def __lt__(self, other):#( <),
-        return 0
+        if isinstance( other, int) and other > 0:
+            return self.number_of_floors < other
+        elif isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
 
     def __le__(self, other): #( <=),
-        return 0
+        if isinstance( other, int) and other > 0:
+            return self.number_of_floors <= other
+        elif isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
 
     def __gt__(self, other): #( >)
-        return 0
+        if isinstance( other, int) and other > 0:
+            return self.number_of_floors > other
+        elif isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
 
     def __ge__(self, other): #( >=)
-        return 0
+        if isinstance( other, int) and other > 0:
+            return self.number_of_floors >= other
+        elif isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
 
-    def __ne__(self, other):
-        return 0  #( !=)
+    def __ne__(self, other):  #( !=)
+        if isinstance( other, int) and other > 0:
+            return self.number_of_floors != other
+        elif isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
 
     def go_to( self, new_floor):
         if isinstance( new_floor, int):
@@ -57,21 +76,22 @@ class House:
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 
-print(h1, h2) # __str__
+print(h1)
+print(h2) # __str__
 print(h1 == h2) # __eq__
 
 h1 = h1 + 10 # __add__
-print("Result ", h1)
+print(h1)
 print(h1 == h2)
 
 h1 += 10 # __iadd__
 print(h1)
-#
-# h2 = 10 + h2 # __radd__
-# print(h2)
-#
-# print(h1 > h2) # __gt__
-# print(h1 >= h2) # __ge__
-# print(h1 < h2) # __lt__
-# print(h1 <= h2) # __le__
-# print(h1 != h2) # __ne__
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
