f12928e3cfeb419632fcbd7c07559bf977c5d75c
AArinova <aliya-arinova@yandex.ru>
2024-08-29 20:41:28 +0700

    Add files via upload

--- module5hard.py
+++ module5hard.py
@@ -1,11 +1,13 @@
 
 class Video:
     def __init__(self, *args, time_now = 0, adult_mode = False):
-        print(args)
         self.title = args[0] #(заголовок, строка),
         self.duration = args[1] #(продолжительность, секунды),
         self.time_now = time_now
         self.adult_mode = adult_mode
+
+    def __str__(self):
+        return f'{self.title}, длительность: {self.duration}, возрастное ограничение: {self.adult_mode}'
 
 class User:
     def __init__(self, *args):
@@ -17,11 +19,10 @@ class User:
         return f'Пользователь: {self.nickname}, пароль: {self.password} возраст: {self.age}'
 
 class UrTube:
-    users = []  # (список  объектов User),
-    videos = []  # (список    объектов    Video),
 
-    #def __init__(self):
-     #   self.current_user = ''  # (текущий    пользователь, User)
+    users = []  # (список  объектов User),
+    videos = []  # (список объектов Video),
+
     def __str__(self):
         return self.current_user
     def register( self, nick_name, password, age):
@@ -33,9 +34,37 @@ class UrTube:
             self.current_user = new_user
         return self
 
+    def add(self, *args):
+        for each in args:
+            if isinstance(each, Video):
+                if each in self.videos:
+                    print('Уже есть видео с таким названием.')
+                else:
+                    self.videos.append(each)
+                    #print(each)
+
 ur = UrTube()
 v1 = Video('Лучший язык программирования 2024 года', 200)
 v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+print(v1)
+# Добавление видео
+ur.add(v1, v2)
+print(ur.videos)
 
+# Проверка поиска
+#print(ur.get_videos('лучший'))
+#print(ur.get_videos('ПРОГ'))
+
+# Проверка на вход пользователя и возрастное ограничение
+# ur.watch_video('Для чего девушкам парень программист?')
+# ur.register('vasya_pupkin', 'lolkekcheburek', 13)
+# ur.watch_video('Для чего девушкам парень программист?')
+# ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
+# ur.watch_video('Для чего девушкам парень программист?')
+
+# Проверка входа в другой аккаунт
 ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
 print(ur.current_user)
+
+# Попытка воспроизведения несуществующего видео
+#ur.watch_video('Лучший язык программирования 2024 года!')
